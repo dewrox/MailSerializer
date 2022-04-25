800ea17a73831605788d49a72ecd6f1910a2ccd4
dewrox <dewm@dewrox.com>
2022-04-25 12:12:21 -0700

    Adding Initial files

---
+++ MailSerializer.cs
@@ -0,0 +1,33 @@
+using MimeKit;
+
+namespace Dewrox.Serializer.Mail
+{
+    public class MailSerializer
+    {
+        public MailSerializer()
+        {
+            
+        }
+        
+        public byte[] Serialize(System.Net.Mail.MailMessage message)
+        {
+            MimeMessage msg = (MimeMessage)message;
+            using MemoryStream ms = new();
+            msg.WriteTo(ms);
+            return ms.ToArray();
+        }
+        
+        public byte[] Serialize(MimeMessage message)
+        {
+            using MemoryStream ms = new();
+            message.WriteTo(ms);
+            return ms.ToArray();
+        }
+        
+        public MimeMessage Deserialize(byte[] msg)
+        {
+            using MemoryStream ms = new(msg);
+            return MimeMessage.Load(ms);
+        }
+    }
+}
